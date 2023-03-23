7b6c185910979bcf4539f074bc31a8116e0c204f
Misiaty-Mat <m.a.s.m@interia.pl>
2023-03-23 17:45:14 +0100

    Image upload (api)

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -12,10 +12,13 @@ namespace Application.Core
             CreateMap<Activity, ActivityDto>()
                 .ForMember(dto => dto.HostUsername, opt => opt.MapFrom(src => src.Attendees
                 .FirstOrDefault(attendee => attendee.IsHost).AppUser.UserName));
-            CreateMap<ActivityAttendee, Profiles.Profile>()
+            CreateMap<ActivityAttendee, AttendeeDto>()
                 .ForMember(destMember => destMember.DisplayName, opt => opt.MapFrom(src => src.AppUser.DisplayName))
                 .ForMember(destMember => destMember.Username, opt => opt.MapFrom(src => src.AppUser.UserName))
-                .ForMember(destMember => destMember.Bio, opt => opt.MapFrom(src => src.AppUser.Bio));
+                .ForMember(destMember => destMember.Bio, opt => opt.MapFrom(src => src.AppUser.Bio))
+                .ForMember(profile => profile.Image, opt => opt.MapFrom(src => src.AppUser.Photos.FirstOrDefault(photo => photo.IsMain).Url));
+            CreateMap<AppUser, Profiles.Profile>()
+                .ForMember(profile => profile.Image, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(photo => photo.IsMain).Url));
         }
     }
 }
