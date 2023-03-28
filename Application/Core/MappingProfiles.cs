088efc46db3ad00fbf43fcc2cec9cd788ceaf554
Misiaty-Mat <m.a.s.m@interia.pl>
2023-03-28 15:40:03 +0200

    Add folow user feature

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -9,6 +9,7 @@ namespace Application.Core
     {
         public MappingProfiles()
         {
+            string currentUsername = null;
             CreateMap<Activity, Activity>();
             CreateMap<Activity, ActivityDto>()
                 .ForMember(dto => dto.HostUsername, opt => opt.MapFrom(src => src.Attendees
@@ -17,9 +18,18 @@ namespace Application.Core
                 .ForMember(destMember => destMember.DisplayName, opt => opt.MapFrom(src => src.AppUser.DisplayName))
                 .ForMember(destMember => destMember.Username, opt => opt.MapFrom(src => src.AppUser.UserName))
                 .ForMember(destMember => destMember.Bio, opt => opt.MapFrom(src => src.AppUser.Bio))
-                .ForMember(profile => profile.Image, opt => opt.MapFrom(src => src.AppUser.Photos.FirstOrDefault(photo => photo.IsMain).Url));
+                .ForMember(profile => profile.Image, opt => 
+                    opt.MapFrom(src => src.AppUser.Photos.FirstOrDefault(photo => photo.IsMain).Url))
+                .ForMember(profile => profile.FollowersCount, opt => opt.MapFrom(src => src.AppUser.Followers.Count))
+                .ForMember(profile => profile.FollowingCount, opt => opt.MapFrom(src => src.AppUser.Followings.Count))
+                .ForMember(profile => profile.Following, opt =>
+                     opt.MapFrom(src => src.AppUser.Followers.Any(user => user.Observer.UserName == currentUsername)));;
             CreateMap<AppUser, Profiles.Profile>()
-                .ForMember(profile => profile.Image, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(photo => photo.IsMain).Url));
+                .ForMember(profile => profile.Image, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(photo => photo.IsMain).Url))
+                .ForMember(profile => profile.FollowersCount, opt => opt.MapFrom(src => src.Followers.Count))
+                .ForMember(profile => profile.FollowingCount, opt => opt.MapFrom(src => src.Followings.Count))
+                .ForMember(profile => profile.Following, opt =>
+                     opt.MapFrom(src => src.Followers.Any(user => user.Observer.UserName == currentUsername)));
             CreateMap<Comment, CommentDto>()
                 .ForMember(destMember => destMember.DisplayName, opt => opt.MapFrom(src => src.Author.DisplayName))
                 .ForMember(destMember => destMember.Username, opt => opt.MapFrom(src => src.Author.UserName))
